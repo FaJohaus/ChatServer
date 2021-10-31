bbc6684dc0d60a6bff6176f3a31bc5a4a7aafb51
21Jon <l.jonas.1@web.de>
2021-10-31 16:27:44 +0100

    done

--- src/com/db/database.java
+++ src/com/db/database.java
@@ -1,12 +1,15 @@
 package com.db;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 /**
  * database
  */
 public class database {
-    static final String URL = String.format("jdbc:mysql://localhost:3306/" + System.getenv("MYSQL_DATABASE")); // "jdbc:mysql://172.30.207.35:1433/userdb";
+    static final String URL = String.format("jdbc:mysql://10.20.0.11:3306/" + System.getenv("MYSQL_DATABASE")); // "jdbc:mysql://172.30.207.35:1433/userdb";
     static final String USER = "root";
     static final String PWD = System.getenv("MYSQL_ROOT_PASSWORD");
 
@@ -15,14 +18,13 @@ public class database {
     public database() {
 
         try {
-            Class.forName("com.mysql.jdbc.Driver");
             // Connect to database
             Connection myConnection = DriverManager.getConnection(URL, USER, PWD);
 
             // Create a statememt
             this.myStatement = myConnection.createStatement();
 
-        } catch (SQLException | ClassNotFoundException throwables) {
+        } catch (SQLException throwables) {
             System.out.println(throwables.getMessage());
             System.err.println("Es konnte keine Verbindung zur Datenbank aufgebaut werden.");
             System.exit(1);
